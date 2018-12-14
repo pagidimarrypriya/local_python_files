7739e9c01ba74d0c2b9e5da5e07308fe94a01ba3
pagidimarrypriya <pagidimarrypriya@gmail.com>
2018-12-14 12:32:51 +0530

    Initial stageing from local machine

---
+++ jsondata_start.py
@@ -0,0 +1,50 @@
+# 
+# Example file for parsing and processing JSON
+#
+import urllib.request 
+import json
+
+def printResults(data):
+  # Use the json module to load the string data into a dictionary
+  theJSON = json.loads(data)
+
+  
+  # now we can access the contents of the JSON like any other Python object
+  if "title" in theJSON["metadata"]:
+    print(theJSON["metadata"]["title"])
+  
+  # output the number of events, plus the magnitude and each event name  
+  
+
+  # for each event, print the place where it occurred
+
+
+  # print the events that only have a magnitude greater than 4
+
+      
+  # print only the events where at least 1 person reported feeling something
+
+  
+def main():
+  # define a variable to hold the source URL
+  # In this case we'll use the free data feed from the USGS
+  # This feed lists all earthquakes for the last day larger than Mag 2.5
+  #urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+  #urlData = "http://earthquake.usgs.gov/earthquakes/feed/"
+  urlData = "http://www.google.com"
+
+  # Open the URL and read the data
+  #webUrl = urllib.request.urlopen(urlData)
+  #print ("result code: " + str(webUrl.getcode()))
+  f = open("metadata.json", "r")
+  printResults(f.read())
+  #data = f.read()
+  f.close()
+  #print(data)
+
+
+
+
+
+if __name__ == "__main__":
+  main()
